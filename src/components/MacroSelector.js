5563e0901522a926bc7637e7c5204aafff5f0d78
Richard Creedon <richardcreedon1989@gmail.com>
2020-12-28 22:02:12 +0000

    further macro selector styling and text added

--- src/components/MacroSelector.js
+++ src/components/MacroSelector.js
@@ -28,7 +28,7 @@ const MacroSelector = (props) => {
     fat && protein && carbs && props.macrosHandler({ protein, carbs, fat });
     parseInt(carbs) + parseInt(protein) + parseInt(fat) !== 100 &&
       toast.error("Must add up to 100%");
-    displayMacroGrams();
+    macroGramsSetter();
   }; //passes macros up to App component to allow for calculation for calories total/remaining etc
 
   useEffect(() => {
@@ -36,7 +36,7 @@ const MacroSelector = (props) => {
     console.log("usefefect");
   }, [props]);
 
-  const displayMacroGrams = (props) => {
+  const macroGramsSetter = (props) => {
     const { protein, fat, carbs } = macro;
     const proteinGrams = (dailyCalories * (protein / 100)) / 4;
     const carbGrams = (dailyCalories * (carbs / 100)) / 4;
@@ -48,25 +48,19 @@ const MacroSelector = (props) => {
     });
   }; //used to calculate the grams for each macro from the total daily calories selected x the macro's %
 
-  const displayProtein = () => {
+  const displayMacroGrams = () => {
     return dailyMacroGrams ? (
-      <div className="total-macro-display">{dailyMacroGrams.protein} Grams</div>
-    ) : (
-      ""
-    );
-  };
+      <div>
+        <p className="total-macro-display">
+          {`Your daily allowance of protein is ${dailyMacroGrams.protein}g. Healthy sources of protein include fish, grains, and chicken.`}
+        </p>
+        <p className="total-macro-display">
+          {" "}
+          {`Your daily allowance of carbohydrates is ${dailyMacroGrams.carbs}g. Healthy sources of carbohydrates include grains, lentils, fruit, and vegetables.`}
+        </p>
 
-  const displayCarbs = () => {
-    return dailyMacroGrams ? (
-      <div className="total-macro-display">{dailyMacroGrams.carbs} Grams</div>
-    ) : (
-      ""
-    );
-  };
-
-  const displayFat = () => {
-    return dailyMacroGrams ? (
-      <div className="total-macro-display">{dailyMacroGrams.fat} Grams</div>
+        <p className="total-macro-display">{`Your daily allowance of fat is ${dailyMacroGrams.fat}g. Healthy sources of fats include avocados, egg yolk, nuts, and seeds `}</p>
+      </div>
     ) : (
       ""
     );
@@ -76,6 +70,25 @@ const MacroSelector = (props) => {
     <div style={{ textAlign: "center" }} className=" macro-setter-parent-div ">
       <h4 className="macro-selector-heading">Macro-nutrient Profile</h4>
       <hr />
+      <div style={{ textAlign: "left", marginLeft: "1em" }}>
+        <p>
+          {" "}
+          Macronutrients are the nutrients we need in larger quantities that
+          provide us with energy.
+        </p>
+        <p>
+          There are seven major classes of nutrients:
+          <ul>
+            <li> Carbohydrates </li>
+            <li> Dietary Fiber </li>
+            <li> Fats </li>
+            <li> Minerals </li>
+            <li> Proteins </li>
+            <li> Vitamins </li>
+            <li> Water </li>
+          </ul>
+        </p>
+      </div>
       <Form className="form-container">
         <div style={{ display: "inline", width: "50%", float: "left" }}>
           <div className="form-group">
@@ -87,10 +100,6 @@ const MacroSelector = (props) => {
               id="protein"
               nutritionHandler={nutritionHandler}
             />
-            <div className="total-macro-display">
-              {" "}
-              {displayProtein(dailyMacroGrams)}{" "}
-            </div>
           </div>
           <div className="form-group">
             <label className="input-label" htmlFor="carbs">
@@ -101,9 +110,6 @@ const MacroSelector = (props) => {
               nutritionHandler={nutritionHandler}
               id="carbs"
             />
-            <div className="total-macro-display">
-              {displayCarbs(dailyMacroGrams)}
-            </div>
           </div>
           <div className="form-group">
             <label className="input-label" htmlFor="fat">
@@ -114,17 +120,15 @@ const MacroSelector = (props) => {
               nutritionHandler={nutritionHandler}
               id="fat"
             />
-            <div className="total-macro-display">
-              {displayFat(dailyMacroGrams)}
-            </div>
           </div>
         </div>
 
         <Button
           style={{
-            marginTop: "3.75em",
+            marginTop: "3.5em",
             display: "inline",
             width: "40%",
+            height: "3em",
           }}
           className="calculate-button"
           id="button"
@@ -133,14 +137,10 @@ const MacroSelector = (props) => {
         >
           Calculate
         </Button>
+
+        {displayMacroGrams(dailyMacroGrams)}
       </Form>
 
-      <div style={{ float: "none", clear: "both" }}>
-        <p> lorem ipsum </p>
-        <p> lorem ipsum </p>
-        <p> lorem ipsum </p>
-        <p> lorem ipsum </p>
-      </div>
       {/* <img className="macro-setter-image" src={macroImage} /> */}
     </div>
   ); // Button above - Enter on fat input clicks calculate button when pass in event + stop refresh + type is submit
