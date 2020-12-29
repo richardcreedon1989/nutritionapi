0743485220bfffa88e1f1573baf7c8288418b72d
Richard Creedon <richardcreedon1989@gmail.com>
2020-12-29 14:58:40 +0000

    removed float for position: absolute to fix image not aligning with sibling div

--- src/components/Slider.js
+++ src/components/Slider.js
@@ -6,56 +6,60 @@ const Sliders = ({ dailyCalorieSelector, setCalorieHandler }) => {
   return (
     <div className="calorie-setter-parent-div text-center font-weight-light">
       <div className="calorie-setter-div">
-        <h4
-          style={{ textAlign: "center", paddingTop: "1em", fontWeight: "bold" }}
-        >
-          Calorie Setter
-        </h4>
+        <h4 className="calorie-setter-heading">Calorie Setter</h4>
         <hr />
-
-        <p>
-          The amount of calories a person burns each day is known as
-          <strong> TDEE </strong> or <i>Total Daily Energy Expenditure </i>.
-        </p>
-        <p>
-          TDEE varies depending on your weight, gender, height, and body
-          composition. To work out your TDEE click
-          <a href="https://tdeecalculator.net/"> here</a>{" "}
-        </p>
-        <hr className="hr-inputs" />
-        <p style={{ textAlign: "center", paddingTop: "1em" }}>
-          Use the slider to increase/decrease the amount of calories you wish to
-          consume per day
-        </p>
-
-        <Label
-          for="volume"
-          style={{ marginLeft: "1em", marginRight: "1em" }}
-        ></Label>
-
-        <input
-          className="slider"
-          type="range"
-          id="volume"
-          name="volume"
-          min="800"
-          max="6000"
-          step="10"
-          value={dailyCalorieSelector}
-          onChange={setCalorieHandler}
-        />
-
-        <h5
+        <div
           style={{
-            display: "inline",
-            marginLeft: "1em",
-            alignContent: "center !important",
+            textAlign: "center",
+            marginLeft: ".5em",
+            marginRight: ".5em",
           }}
         >
-          {dailyCalorieSelector}
-        </h5>
+          <p>
+            The amount of calories a person burns each day varies depending on
+            your weight, gender, height, and body composition. To work out your
+            TDEE click
+            <a href="https://tdeecalculator.net/"> here</a> .
+          </p>
+
+          <hr className="hr-inputs" />
+          <p
+            style={{
+              textAlign: "center",
+              paddingTop: "1em",
+            }}
+          >
+            Use the slider to increase/decrease the amount of calories you wish
+            to consume per day
+          </p>
+        </div>
+        <div style={{ textAlign: "center" }}>
+          <Label for="volume"></Label>
+
+          <input
+            className="slider"
+            type="range"
+            id="volume"
+            name="volume"
+            min="800"
+            max="6000"
+            step="10"
+            value={dailyCalorieSelector}
+            onChange={setCalorieHandler}
+          />
+
+          <h5
+            style={{
+              display: "inline",
+              marginLeft: "1em",
+              alignContent: "center !important",
+            }}
+          >
+            {dailyCalorieSelector}
+          </h5>
+        </div>
       </div>
-      <div className="inline-display">
+      <div className="image-div">
         <img className="calorie-slider-image" src={calorieSelectorImage} />{" "}
       </div>
     </div>
